96a2377bc89d3064605893291ca737093af7b438
Paolo Meridiani <sha1-fdf6ab94344596a1fb314eacb08f7a91de35b555@cern.ch>
2006-03-14 15:53:51 +0000

    First release

--- test/testEcalDetId.cc
+++ test/testEcalDetId.cc
@@ -2,18 +2,19 @@
    \file
    Test suit for EcalDetId
 
-   \version $Id: testEcalDetId.cc,v 1.5 2005/12/19 17:50:44 rahatlou Exp $
+   \version $Id: testEcalDetId.cc,v 1.6 2006/02/27 18:59:15 meridian Exp $
 
    \note This test is not exaustive     
 */
 
-static const char CVSId[] = "$Id: testEcalDetId.cc,v 1.5 2005/12/19 17:50:44 rahatlou Exp $";
+static const char CVSId[] = "$Id: testEcalDetId.cc,v 1.6 2006/02/27 18:59:15 meridian Exp $";
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <DataFormats/EcalDetId/interface/EBDetId.h>
 #include <DataFormats/EcalDetId/interface/EEDetId.h>
 #include <DataFormats/EcalDetId/interface/ESDetId.h>
 #include <DataFormats/EcalDetId/interface/EcalTrigTowerDetId.h>
+#include <DataFormats/EcalDetId/interface/EcalElectronicsId.h>
 #include <DataFormats/EcalDetId/interface/EcalPnDiodeDetId.h>
 
 #include <iostream>
@@ -26,6 +27,7 @@ class testEcalDetId: public CppUnit::TestFixture {
   CPPUNIT_TEST(testEEDetId);
   CPPUNIT_TEST(testESDetId);
   CPPUNIT_TEST(testEcalTrigTowerDetId);
+  CPPUNIT_TEST(testEcalElectronicsId);
   CPPUNIT_TEST(testPnDiodeDetId);
 
   CPPUNIT_TEST_SUITE_END();
@@ -39,6 +41,7 @@ public:
   void testEEDetId();
   void testESDetId();
   void testEcalTrigTowerDetId();
+  void testEcalElectronicsId();
   void testPnDiodeDetId();
  
 }; 
@@ -51,47 +54,54 @@ void testEcalDetId::testEBDetId(){
   EBDetId smId;
 
   for (int ieta=EBDetId::MIN_IETA;ieta<=EBDetId::MAX_IETA;ieta++)
-      for (int iphi=EBDetId::MIN_IPHI;iphi<=EBDetId::MAX_IPHI;iphi++)
-	{
-	  //EBDetId Zside 1 
+    for (int iphi=EBDetId::MIN_IPHI;iphi<=EBDetId::MAX_IPHI;iphi++)
+      {
+	//EBDetId Zside 1 
+	try
 	  {
-	    EBDetId aPositiveId(ieta,iphi);
-	    CPPUNIT_ASSERT(aPositiveId.ieta()==ieta);
-	    CPPUNIT_ASSERT(aPositiveId.iphi()==iphi);
-	    CPPUNIT_ASSERT(aPositiveId.zside()==1);
-	    CPPUNIT_ASSERT(aPositiveId.ietaAbs()==ieta);
+	    {
+	      EBDetId aPositiveId(ieta,iphi);
+	      CPPUNIT_ASSERT(aPositiveId.ieta()==ieta);
+	      CPPUNIT_ASSERT(aPositiveId.iphi()==iphi);
+	      CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	      CPPUNIT_ASSERT(aPositiveId.ietaAbs()==ieta);
 
-	    smId = EBDetId(aPositiveId.ism(), aPositiveId.ic(),
-			   EBDetId::SMCRYSTALMODE);
-	    CPPUNIT_ASSERT(aPositiveId==smId);
-	    CPPUNIT_ASSERT(aPositiveId.ism()==smId.ism());
-	    CPPUNIT_ASSERT(aPositiveId.ic()==smId.ic());
-	    CPPUNIT_ASSERT(smId.ism()>=EBDetId::MIN_SM);
-	    CPPUNIT_ASSERT(smId.ism()<=EBDetId::MAX_SM);
-	    CPPUNIT_ASSERT(smId.ic()>=EBDetId::MIN_C);
-	    CPPUNIT_ASSERT(smId.ic()<=EBDetId::MAX_C);
+	      smId = EBDetId(aPositiveId.ism(), aPositiveId.ic(),
+			     EBDetId::SMCRYSTALMODE);
+	      CPPUNIT_ASSERT(aPositiveId==smId);
+	      CPPUNIT_ASSERT(aPositiveId.ism()==smId.ism());
+	      CPPUNIT_ASSERT(aPositiveId.ic()==smId.ic());
+	      CPPUNIT_ASSERT(smId.ism()>=EBDetId::MIN_SM);
+	      CPPUNIT_ASSERT(smId.ism()<=EBDetId::MAX_SM);
+	      CPPUNIT_ASSERT(smId.ic()>=EBDetId::MIN_C);
+	      CPPUNIT_ASSERT(smId.ic()<=EBDetId::MAX_C);
+	    }
+
+	    //EBDetId Zside -1 
+	    {
+	      EBDetId aNegativeId(-1*ieta,iphi);
+	      CPPUNIT_ASSERT(aNegativeId.ieta()==-1*ieta);
+	      CPPUNIT_ASSERT(aNegativeId.iphi()==iphi);
+	      CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	      CPPUNIT_ASSERT(aNegativeId.ietaAbs()==ieta);
+
+	      smId = EBDetId(aNegativeId.ism(), aNegativeId.ic(),
+			     EBDetId::SMCRYSTALMODE);
+	      CPPUNIT_ASSERT(aNegativeId==smId);
+	      CPPUNIT_ASSERT(aNegativeId.ism()==smId.ism());
+	      CPPUNIT_ASSERT(aNegativeId.ic()==smId.ic());
+	      CPPUNIT_ASSERT(smId.ism()>=EBDetId::MIN_SM);
+	      CPPUNIT_ASSERT(smId.ism()<=EBDetId::MAX_SM);
+	      CPPUNIT_ASSERT(smId.ic()>=EBDetId::MIN_C);
+	      CPPUNIT_ASSERT(smId.ic()<=EBDetId::MAX_C);
+
+	    }
 	  }
-
-	  //EBDetId Zside -1 
-	  {
-	    EBDetId aNegativeId(-1*ieta,iphi);
-	    CPPUNIT_ASSERT(aNegativeId.ieta()==-1*ieta);
-	    CPPUNIT_ASSERT(aNegativeId.iphi()==iphi);
-	    CPPUNIT_ASSERT(aNegativeId.zside()==-1);
-	    CPPUNIT_ASSERT(aNegativeId.ietaAbs()==ieta);
-
-	    smId = EBDetId(aNegativeId.ism(), aNegativeId.ic(),
-			   EBDetId::SMCRYSTALMODE);
-	    CPPUNIT_ASSERT(aNegativeId==smId);
-	    CPPUNIT_ASSERT(aNegativeId.ism()==smId.ism());
-	    CPPUNIT_ASSERT(aNegativeId.ic()==smId.ic());
-	    CPPUNIT_ASSERT(smId.ism()>=EBDetId::MIN_SM);
-	    CPPUNIT_ASSERT(smId.ism()<=EBDetId::MAX_SM);
-	    CPPUNIT_ASSERT(smId.ic()>=EBDetId::MIN_C);
-	    CPPUNIT_ASSERT(smId.ic()<=EBDetId::MAX_C);
-
+	catch ( std::runtime_error &e ) 
+	  { 
 	  }
-	}
+      }
+
   
 }
 
@@ -101,20 +111,26 @@ void testEcalDetId::testEEDetId(){
   for (int ix=EEDetId::IX_MIN;ix<=EEDetId::IX_MAX;ix++)
     for (int iy=EEDetId::IY_MIN;iy<=EEDetId::IY_MAX;iy++)
       {
-	//EEDetId Zside 1 
-	{
-	  EEDetId aPositiveId(ix,iy,1);
-	  CPPUNIT_ASSERT(aPositiveId.ix()==ix);
-	  CPPUNIT_ASSERT(aPositiveId.iy()==iy);
-	  CPPUNIT_ASSERT(aPositiveId.zside()==1);
-	}
-	//EEDetId Zside -1 
-	{
-	  EEDetId aNegativeId(ix,iy,-1);
-	  CPPUNIT_ASSERT(aNegativeId.ix()==ix);
-	  CPPUNIT_ASSERT(aNegativeId.iy()==iy);
-	  CPPUNIT_ASSERT(aNegativeId.zside()==-1);
-	}
+	try
+	  {
+	    //EEDetId Zside 1 
+	    {
+	      EEDetId aPositiveId(ix,iy,1);
+	      CPPUNIT_ASSERT(aPositiveId.ix()==ix);
+	      CPPUNIT_ASSERT(aPositiveId.iy()==iy);
+	      CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	    }
+	    //EEDetId Zside -1 
+	    {
+	      EEDetId aNegativeId(ix,iy,-1);
+	      CPPUNIT_ASSERT(aNegativeId.ix()==ix);
+	      CPPUNIT_ASSERT(aNegativeId.iy()==iy);
+	      CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	    }
+	  }
+	catch ( std::runtime_error &e ) 
+	  { 
+	  }
       }
   
 }
@@ -125,95 +141,128 @@ void testEcalDetId::testESDetId(){
   for (int istrip=ESDetId::ISTRIP_MIN;istrip<=ESDetId::ISTRIP_MAX;istrip++)
     for (int ix=ESDetId::IX_MIN;ix<=ESDetId::IX_MAX;ix++)
       for (int iy=ESDetId::IY_MIN;iy<=ESDetId::IY_MAX;iy++)
-      {
-	//ESDetId Plane 0 Zside 1 
 	{
-	  ESDetId aPositiveId(istrip,ix,iy,1,1);
-	  CPPUNIT_ASSERT(aPositiveId.strip()==istrip);
-	  CPPUNIT_ASSERT(aPositiveId.six()==ix);
-	  CPPUNIT_ASSERT(aPositiveId.siy()==iy);
-	  CPPUNIT_ASSERT(aPositiveId.plane()==1);
-	  CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	  try 
+	    {
+	      //ESDetId Plane 0 Zside 1 
+	      {
+		ESDetId aPositiveId(istrip,ix,iy,1,1);
+		CPPUNIT_ASSERT(aPositiveId.strip()==istrip);
+		CPPUNIT_ASSERT(aPositiveId.six()==ix);
+		CPPUNIT_ASSERT(aPositiveId.siy()==iy);
+		CPPUNIT_ASSERT(aPositiveId.plane()==1);
+		CPPUNIT_ASSERT(aPositiveId.zside()==1);
 
+	      }
+	      //ESDetId Plane 1 Zside 1 
+	      {
+		ESDetId aPositiveId(istrip,ix,iy,2,1);
+		CPPUNIT_ASSERT(aPositiveId.strip()==istrip);
+		CPPUNIT_ASSERT(aPositiveId.six()==ix);
+		CPPUNIT_ASSERT(aPositiveId.siy()==iy);
+		CPPUNIT_ASSERT(aPositiveId.plane()==2);
+		CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	      }
+	      //ESDetId Plane 0 Zside -1 
+	      {
+		ESDetId aNegativeId(istrip,ix,iy,1,-1);
+		CPPUNIT_ASSERT(aNegativeId.strip()==istrip);
+		CPPUNIT_ASSERT(aNegativeId.six()==ix);
+		CPPUNIT_ASSERT(aNegativeId.siy()==iy);
+		CPPUNIT_ASSERT(aNegativeId.plane()==1);
+		CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	      }
+	      //ESDetId Plane 1 Zside -1 
+	      {
+		ESDetId aNegativeId(istrip,ix,iy,2,-1);
+		CPPUNIT_ASSERT(aNegativeId.strip()==istrip);
+		CPPUNIT_ASSERT(aNegativeId.six()==ix);
+		CPPUNIT_ASSERT(aNegativeId.siy()==iy);
+		CPPUNIT_ASSERT(aNegativeId.plane()==2);
+		CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	      }
+	    }
+	  catch ( std::runtime_error &e ) 
+	    { 
+	    }
 	}
-	//ESDetId Plane 1 Zside 1 
-	{
-	  ESDetId aPositiveId(istrip,ix,iy,2,1);
-	  CPPUNIT_ASSERT(aPositiveId.strip()==istrip);
-	  CPPUNIT_ASSERT(aPositiveId.six()==ix);
-	  CPPUNIT_ASSERT(aPositiveId.siy()==iy);
-	  CPPUNIT_ASSERT(aPositiveId.plane()==2);
-	  CPPUNIT_ASSERT(aPositiveId.zside()==1);
-	}
-	//ESDetId Plane 0 Zside -1 
-	{
-	  ESDetId aNegativeId(istrip,ix,iy,1,-1);
-	  CPPUNIT_ASSERT(aNegativeId.strip()==istrip);
-	  CPPUNIT_ASSERT(aNegativeId.six()==ix);
-	  CPPUNIT_ASSERT(aNegativeId.siy()==iy);
-	  CPPUNIT_ASSERT(aNegativeId.plane()==1);
-	  CPPUNIT_ASSERT(aNegativeId.zside()==-1);
-	}
-	//ESDetId Plane 1 Zside -1 
-	{
-	  ESDetId aNegativeId(istrip,ix,iy,2,-1);
-	  CPPUNIT_ASSERT(aNegativeId.strip()==istrip);
-	  CPPUNIT_ASSERT(aNegativeId.six()==ix);
-	  CPPUNIT_ASSERT(aNegativeId.siy()==iy);
-	  CPPUNIT_ASSERT(aNegativeId.plane()==2);
-	  CPPUNIT_ASSERT(aNegativeId.zside()==-1);
-	}
-      }
-  
 }
 
 
 void testEcalDetId::testEcalTrigTowerDetId() {
 
   for (int ieta=EcalTrigTowerDetId::MIN_I;ieta<=EcalTrigTowerDetId::MAX_I;ieta++)
-      for (int iphi=EcalTrigTowerDetId::MIN_I;iphi<=EcalTrigTowerDetId::MAX_I;iphi++)
-	{
-	  //EcalTrigTowerDetId Zside 1 
+    for (int iphi=EcalTrigTowerDetId::MIN_I;iphi<=EcalTrigTowerDetId::MAX_I;iphi++)
+      {
+	try
 	  {
-	    EcalTrigTowerDetId aPositiveId(1,EcalBarrel,ieta,iphi);
-	    CPPUNIT_ASSERT(aPositiveId.subDet()==EcalBarrel);
-	    CPPUNIT_ASSERT(aPositiveId.ieta()==ieta);
-	    CPPUNIT_ASSERT(aPositiveId.iphi()==iphi);
-	    CPPUNIT_ASSERT(aPositiveId.zside()==1);
-	    CPPUNIT_ASSERT(aPositiveId.ietaAbs()==ieta);
+	    //EcalTrigTowerDetId Zside 1 
+	    {
+	      EcalTrigTowerDetId aPositiveId(1,EcalBarrel,ieta,iphi);
+	      CPPUNIT_ASSERT(aPositiveId.subDet()==EcalBarrel);
+	      CPPUNIT_ASSERT(aPositiveId.ieta()==ieta);
+	      CPPUNIT_ASSERT(aPositiveId.iphi()==iphi);
+	      CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	      CPPUNIT_ASSERT(aPositiveId.ietaAbs()==ieta);
+	    }
+	    //EcalTrigTowerDetId Zside -1 
+	    {
+	      EcalTrigTowerDetId aNegativeId(-1,EcalBarrel,ieta,iphi);
+	      CPPUNIT_ASSERT(aNegativeId.subDet()==EcalBarrel);
+	      CPPUNIT_ASSERT(aNegativeId.ieta()==-1*ieta);
+	      CPPUNIT_ASSERT(aNegativeId.iphi()==iphi);
+	      CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	      CPPUNIT_ASSERT(aNegativeId.ietaAbs()==ieta);
+	    }
 	  }
-	  //EcalTrigTowerDetId Zside -1 
-	  {
-	    EcalTrigTowerDetId aNegativeId(-1,EcalBarrel,ieta,iphi);
-	    CPPUNIT_ASSERT(aNegativeId.subDet()==EcalBarrel);
-	    CPPUNIT_ASSERT(aNegativeId.ieta()==-1*ieta);
-	    CPPUNIT_ASSERT(aNegativeId.iphi()==iphi);
-	    CPPUNIT_ASSERT(aNegativeId.zside()==-1);
-	    CPPUNIT_ASSERT(aNegativeId.ietaAbs()==ieta);
+	catch ( std::runtime_error &e ) 
+	  { 
 	  }
-	}
-
+      }
   for (int ix=EcalTrigTowerDetId::MIN_I;ix<=EcalTrigTowerDetId::MAX_I;ix++)
-      for (int iy=EcalTrigTowerDetId::MIN_I;iy<=EcalTrigTowerDetId::MAX_I;iy++)
-	{
-	  //EcalTrigTowerDetId Zside 1 
+    for (int iy=EcalTrigTowerDetId::MIN_I;iy<=EcalTrigTowerDetId::MAX_I;iy++)
+      {
+	try
 	  {
-	    EcalTrigTowerDetId aPositiveId(1,EcalEndcap,ix,iy);
-	    CPPUNIT_ASSERT(aPositiveId.ix()==ix);
-	    CPPUNIT_ASSERT(aPositiveId.subDet()==EcalEndcap);
-	    CPPUNIT_ASSERT(aPositiveId.iy()==iy);
-	    CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	    //EcalTrigTowerDetId Zside 1 
+	    {
+	      EcalTrigTowerDetId aPositiveId(1,EcalEndcap,ix,iy);
+	      CPPUNIT_ASSERT(aPositiveId.ix()==ix);
+	      CPPUNIT_ASSERT(aPositiveId.subDet()==EcalEndcap);
+	      CPPUNIT_ASSERT(aPositiveId.iy()==iy);
+	      CPPUNIT_ASSERT(aPositiveId.zside()==1);
+	    }
+	    //EcalTrigTowerDetId Zside -1 
+	    {
+	      EcalTrigTowerDetId aNegativeId(-1,EcalEndcap,ix,iy);
+	      CPPUNIT_ASSERT(aNegativeId.subDet()==EcalEndcap);
+	      CPPUNIT_ASSERT(aNegativeId.ix()==ix);
+	      CPPUNIT_ASSERT(aNegativeId.iy()==iy);
+	      CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	    }
 	  }
-	  //EcalTrigTowerDetId Zside -1 
-	  {
-	    EcalTrigTowerDetId aNegativeId(-1,EcalEndcap,ix,iy);
-	    CPPUNIT_ASSERT(aNegativeId.subDet()==EcalEndcap);
-	    CPPUNIT_ASSERT(aNegativeId.ix()==ix);
-	    CPPUNIT_ASSERT(aNegativeId.iy()==iy);
-	    CPPUNIT_ASSERT(aNegativeId.zside()==-1);
+	catch ( std::runtime_error &e ) 
+	  { 
 	  }
-	}
+      }
+}
 
+void testEcalDetId::testEcalElectronicsId() {
+  for (int idcc=EcalElectronicsId::MIN_DCCID;idcc<=EcalElectronicsId::MAX_DCCID;idcc++)
+    for (int itower=EcalElectronicsId::MIN_TOWERID;itower<EcalElectronicsId::MAX_TOWERID;itower++)
+      for (int ichannel=EcalElectronicsId::MIN_CHANNELID;ichannel<EcalElectronicsId::MAX_CHANNELID;ichannel++)
+	{
+	  try
+	    {
+	      EcalElectronicsId electronicsId(idcc,itower,ichannel);
+	      CPPUNIT_ASSERT(electronicsId.dccId()==idcc);
+	      CPPUNIT_ASSERT(electronicsId.towerId()==itower);
+	      CPPUNIT_ASSERT(electronicsId.channelId()==ichannel);
+	    }
+	  catch ( std::runtime_error &e ) 
+	    { 
+	    }
+	}
 }
 
 void testEcalDetId::testPnDiodeDetId() {
@@ -222,12 +271,18 @@ void testEcalDetId::testPnDiodeDetId() {
     for (int iDCC=1;iDCC<=36;iDCC++)
       for (int iPn=1;iPn<=10;iPn++)
 	{
-	  //EcalTrigTowerDetId Zside 1 
-	  {
-	    EcalPnDiodeDetId aId(iEcalSubDet,iDCC,iPn);
-	    CPPUNIT_ASSERT(aId.iEcalSubDetectorId()==iEcalSubDet);
-	    CPPUNIT_ASSERT(aId.iDCCId()==iDCC);
-	    CPPUNIT_ASSERT(aId.iPnId()==iPn);
-	  }
+	  try
+	    {
+	      //EcalTrigTowerDetId Zside 1 
+	      {
+		EcalPnDiodeDetId aId(iEcalSubDet,iDCC,iPn);
+		CPPUNIT_ASSERT(aId.iEcalSubDetectorId()==iEcalSubDet);
+		CPPUNIT_ASSERT(aId.iDCCId()==iDCC);
+		CPPUNIT_ASSERT(aId.iPnId()==iPn);
+	      }
+	    }
+	  catch ( std::runtime_error &e ) 
+	    { 
+	    }
 	}
 }
